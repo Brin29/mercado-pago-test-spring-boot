a4be9fc1c2ca862e4907d167ff50ba2784812084
Brin29 <breinerstivenparracortes50@gmail.com>
2024-10-16 16:09:09 -0500

    comenzando con la configuracion de la api

--- src/main/java/prueba/products/Controllers/ProductController.java
+++ src/main/java/prueba/products/Controllers/ProductController.java
@@ -1,7 +1,12 @@
 package prueba.products.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import prueba.products.Entity.Product;
 import prueba.products.Repository.ProductRepository;
+
+import java.util.List;
 
 public class ProductController {
 
@@ -13,5 +18,12 @@ public class ProductController {
 
 
     @PostMapping("/add")
-    public
+    public Product product(@RequestBody Product newProduct){
+        return repository.save(newProduct);
+    }
+
+    @GetMapping("/product")
+    public List<Product> all(){
+        return repository.findAll();
+    }
 }
